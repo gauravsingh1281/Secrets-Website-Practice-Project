6ba6ed1315fe97fda180e7f6e40a4de93c2d4288
Gaurav Pratap Singh <gs492979@gmail.com>
2023-02-21 16:42:51 +0530

    Authentication security increased to level 3 using md5 hashing.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 const app = express();
 const port = 3000;
 
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET,encryptedFields: ["password"] });
+
 
 const User = new mongoose.model("User", userSchema);
 
@@ -36,7 +36,7 @@ app.get("/register", (req, res) => {
 app.post("/register", (req, res) => {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password,
+    password: md5(req.body.password),
   });
 
   newUser.save((e) => {
@@ -50,7 +50,7 @@ app.post("/register", (req, res) => {
 
 app.post("/login", (req, res) => {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({ email: username }, (e, foundUser) => {
     if (e) {
